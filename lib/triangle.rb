d67bfca06ea5dec08b726cf9d2c722bdc87fae0d
jaspercurry <jaspercurry@gmail.com>
2016-08-01 21:44:24 -0400

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+
+
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1.to_f
+    @side2 = side2.to_f
+    @side3 = side3.to_f
+  end
+
+  def kind
+    tricheck
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    elsif side1 != side2 && side2 != side3 && side1 != side3
+      :scalene
+    else
+      binding.pry
+      raise TriangleError
+    end
+  end
+
+  def tricheck
+    if side1 == 0 || side2 == 0 || side3 == 0
+      raise TriangleError
+    elsif side1 < 0 || side2 < 0 || side3 < 0
+      raise TriangleError
+    elsif side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2
+      #elsif side1 + side2 < side3 || side2 + side3 < side1 || side3 + side1 < side2
+      # raise TraingleError does not work - why?
+      nil
+    else
+      raise TriangleError
+    end
+  end
+end
+
+
+
+class TriangleError < StandardError
 end
